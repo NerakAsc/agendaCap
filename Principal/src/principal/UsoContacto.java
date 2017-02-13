aacd92b6c8ad20de88bae944bb5f284e202a7e95
NerakAsc <anniascencio23@gmail.com>
2017-02-13 10:34:03 -0600

    Update UsoContacto.java
    se modifico el menu by padilla

--- Principal/src/principal/UsoContacto.java
+++ Principal/src/principal/UsoContacto.java
@@ -12,7 +12,6 @@ public class UsoContacto extends MetodosPrincipal{//extendemos de la clase Metod
 				int opcion_menu = menuPrincipal();//metodo que extrae el menu principal y la respuesta de la clase MetodosPrincipal
 				switch(opcion_menu) {
 					case 1://opcion para agregar datos
-						
 						int opcionContacto = menuContacto();//metodo que extrae el menu y la respuesta del contacto a agregar de la clase MetodosPrincipal
 						agregar(opcionContacto);//metodo: recibe la respuesta con la cual agrega el tipo de contacto en la clase MetodosPrincipal
 						break;
@@ -20,22 +19,21 @@ public class UsoContacto extends MetodosPrincipal{//extendemos de la clase Metod
 						mostrar();//metodo: extrae metodos de cada contacto para mostrar todos los contactos
 						break;
 					case 3://opcion para borrar datos
-	
-						opcionContacto=menuContacto();
+                                                opcionContacto =menuContacto();
 						borrar(opcionContacto);//metodo: extrae metodos de cada contacto para borrar por tipo de contacto y su nombre 
 						break;
 					case 4://opcion para remplazar algun dato por tipo de contacto
-						//remplazar();
+						remplazar();
 						break;
 					case 5://opcion para salir de la aplicacion
 						salir=false;
-						System.out.println("¡Aplicacion terminada!");
+						System.out.println("Â¡Aplicacion terminada!");
 						break;
 				}
 			}
 		}
 		catch(Exception e){
-			System.out.println("¡Para los menus solo se admiten numeros!");
+			System.out.println("Â¡Para los menus solo se admiten numeros!");
 			UsoContacto.main(null);
 		}
 	}
